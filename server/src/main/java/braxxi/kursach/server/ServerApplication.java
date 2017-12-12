eafdf28d8f0239704d4bed098c36f17ee3565f20
shvedov.andrey <ashvedov@logic-way.com>
2017-12-12 22:00:31 +0300

    part5

--- server/src/main/java/braxxi/kursach/server/ServerApplication.java
+++ server/src/main/java/braxxi/kursach/server/ServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KursachApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KursachApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 }
--- server/src/test/java/braxxi/kursach/ServerApplicationTests.java
+++ server/src/test/java/braxxi/kursach/ServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class KursachApplicationTests {
+public class ServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
